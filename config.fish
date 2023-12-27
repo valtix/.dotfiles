70b7c8eb451c504a6cc26945b82e78ce7921b68a
Valtierrez <ivan.valtierrez@gmail.com>
2023-12-27 11:46:18 -0600

    adding new aliases

--- config.fish
+++ config.fish
@@ -2,6 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-
-alias lt='exa --tree --level=2'
-alias l="exa --tree --level=2 -a --icons"
+alias lt="exa --tree --level=2 -a --icons"
+alias l="exa --tree --level=1 --icons"
+alias la="exa --header --long --all"
