f2c59c68a29ede483cb2ff1d581876ad24f4c0d6
Ivan <ivan.valtierrez@gmail.com>
2023-12-28 09:16:29 -0600

    Update config.fish

--- config.fish
+++ config.fish
@@ -5,3 +5,8 @@ end
 alias lt="exa --tree --level=2 -a --icons"
 alias l="exa --tree --level=1 --icons"
 alias la="exa --header --long --all"
+
+alias lh="cd ~"
+alias dt="cd ~/.dotfiles/"
+alias web="cd ~/Documents/webDevelopmentCourse"
+alias wd="cd /Users/ivaltier/Library/CloudStorage/WorkDocsDrive-Documents/"     # work labtop ONLY
